d684610aaa2150b6ea1f0546578f59c45a8ab655
Guilherme Barros <wingui3@gmail.com>
2022-05-12 16:09:10 -0300

    :sparkles: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+
 export const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };
