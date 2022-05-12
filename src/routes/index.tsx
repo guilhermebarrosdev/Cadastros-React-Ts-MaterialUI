af9ac4b6e5431d8ba4fcb54e08cbcb01fe8447ca
Guilherme Barros <wingui3@gmail.com>
2022-05-12 16:43:01 -0300

    :sparkles: Adicionado materialUi com tema claro e provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,17 @@
+import { Button } from '@mui/material';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            Teste
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
