ff5d2714ff817e9f20ea102df6c55042c58555bb
Guilherme Barros <wingui3@gmail.com>
2022-05-20 09:50:32 -0300

    :sparkles:  Adicionado hook de useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,20 +1,24 @@
 import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { FerramentasDaListagem } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { LayoutBasePage } from '../../shared/layouts';
 import { PessoasServices } from '../../shared/services/api/pessoas/PessoaService';
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000, false);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasServices.getAll(1, busca).then((result) => {
-      if (result instanceof Error) alert(result.message);
-      else console.log(result);
+    debounce(() => {
+      PessoasServices.getAll(1, busca).then((result) => {
+        if (result instanceof Error) alert(result.message);
+        else console.log(result);
+      });
     });
   }, [busca]);
 
