c365fd7efc62fee78b6b37d45f8383b5327844b1
Guilherme Barros <wingui3@gmail.com>
2022-05-24 01:15:59 -0300

    :sparkles: Adicionado consulta no backend e metodo de apagar registro na tela de detalhes de pessoas

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,15 +1,50 @@
+import { LinearProgress } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { FerramentasDeDetalhes } from '../../shared/components';
 import { LayoutBasePage } from '../../shared/layouts';
+import { PessoasService } from '../../shared/services/api/pessoas/PessoaService';
 
 export const DetalheDePessoas: React.FC = () => {
   const { id = 'nova' } = useParams<'id'>();
   const navigate = useNavigate();
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    if (id !== 'nova') {
+      setIsLoading(true);
+      PessoasService.getById(+id).then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+          navigate('/pessoas');
+        } else {
+          setName(result.nomeCompleto);
+          console.log(result);
+        }
+      });
+    }
+  }, [id]);
+
+  const handleDelete = (id: number) => {
+    if (confirm('Realmente deseja apagar?')) {
+      PessoasService.deleteById(id).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          alert('Registro apagado com sucesso!');
+          navigate('/pessoas');
+        }
+      });
+    }
+  };
+
   return (
     <LayoutBasePage
-      titulo="Detalhe de pessoas"
+      titulo={id === 'nova' ? 'Nova Pessoa' : name}
       barraDeFerramentas={
         <FerramentasDeDetalhes
           textoBotaoNovo="Nova"
@@ -18,12 +53,13 @@ export const DetalheDePessoas: React.FC = () => {
           mostrarBotaoNovo={id !== 'nova'}
           aoClicarEmSalvar={() => {}}
           aoClicarEmSalvarEFechar={() => {}}
-          aoClicarEmApagar={() => {}}
+          aoClicarEmApagar={() => handleDelete(+id)}
           aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
           aoClicarEmVoltar={() => navigate('/pessoas')}
         />
       }
     >
+      {isLoading && <LinearProgress variant="indeterminate" />}
       <p>DetalheDePessoas {id}</p>
     </LayoutBasePage>
   );
